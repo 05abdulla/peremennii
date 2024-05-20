9db0edd90d53fcb11be559101399caa88f4a743c
05abdulla <05abdulla2@gmail.com>
2024-05-21 00:32:43 +0300

    Add files via upload

---
+++ Peremennii.py
@@ -0,0 +1,9 @@
+fist_name = 'Vsego zadach:'
+zadach = 12
+second_name = 'Zatracheno chasov:'
+chasov = 1.5
+third_name = 'Kurs: Python'
+potracheno_na_odno_zadanie = 1.5 / 12
+fourth_name = 'srednee vremya vipolneniya'
+fifth_name = 'chasov'
+print(third_name, fist_name, zadach, second_name, chasov, fourth_name, potracheno_na_odno_zadanie, fifth_name)
